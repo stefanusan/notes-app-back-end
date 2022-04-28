b144999be44258c70d03a83b542da705d13b6973
Stefa <stefanus.adi@ti.ukdw.ac.id>
2022-04-28 11:03:59 +0700

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -130,26 +130,26 @@
 // };
 
 const ClientError = require('../../exceptions/ClientError');
- 
+
 class NotesHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
- 
+
     this.postNoteHandler = this.postNoteHandler.bind(this);
     this.getNotesHandler = this.getNotesHandler.bind(this);
     this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
     this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
- 
-  postNoteHandler(request, h) {
+
+  async postNoteHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
- 
-      const noteId = this._service.addNote({ title, body, tags });
- 
+
+      const noteId = await this._service.addNote({ title, body, tags });
+
       const response = h.response({
         status: 'success',
         message: 'Catatan berhasil ditambahkan',
@@ -168,7 +168,7 @@ class NotesHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -179,9 +179,9 @@ class NotesHandler {
       return response;
     }
   }
- 
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
       status: 'success',
       data: {
@@ -189,11 +189,11 @@ class NotesHandler {
       },
     };
   }
- 
-  getNoteByIdHandler(request, h) {
+
+  async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      const note = this._service.getNoteById(id);
+      const note = await this._service.getNoteById(id);
       return {
         status: 'success',
         data: {
@@ -209,7 +209,7 @@ class NotesHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -220,14 +220,15 @@ class NotesHandler {
       return response;
     }
   }
- 
-  putNoteByIdHandler(request, h) {
+
+  async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload);
+      const { title, body, tags } = request.payload;
       const { id } = request.params;
- 
-      this._service.editNoteById(id, request.payload);
- 
+
+      await this._service.editNoteById(id, { title, body, tags });
+
       return {
         status: 'success',
         message: 'Catatan berhasil diperbarui',
@@ -241,7 +242,7 @@ class NotesHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -252,12 +253,12 @@ class NotesHandler {
       return response;
     }
   }
- 
-  deleteNoteByIdHandler(request, h) {
+
+  async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params;
-      this._service.deleteNoteById(id);
- 
+      await this._service.deleteNoteById(id);
+
       return {
         status: 'success',
         message: 'Catatan berhasil dihapus',
@@ -271,7 +272,7 @@ class NotesHandler {
         response.code(error.statusCode);
         return response;
       }
- 
+
       // Server ERROR!
       const response = h.response({
         status: 'error',
@@ -283,5 +284,5 @@ class NotesHandler {
     }
   }
 }
- 
+
 module.exports = NotesHandler;
